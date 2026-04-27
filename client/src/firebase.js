7baddc97726c6c81b116fc6d31460689348d43e8
Praneeth <yelletipraneeth16@gmail.com>
2026-04-27 21:37:28 +0530

    Add Firebase config values from Firebase console

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -14,7 +14,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: "G-F24Z7RLZYG"
 };
 
 // Initialize Firebase
